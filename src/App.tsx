84c1ee492dfab7a558676558bd99503b4108cd26
Samuel-CHAU <d271059036999@gmail.com>
2024-03-22 00:20:59 +0800

    use hashRouter to fix the routing bug on Github

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route, HashRouter} from "react-router-dom";
 import Home from "../src/pages/homePage/home";
 import AboutMe from "../src/pages/aboutMePage/aboutMe"
 
 function App() {
   return (
-      <BrowserRouter>
+      //Remark: Deploy on Github using browserRouter will lead to routing bug
+      //use HashRouter instead
+      // <BrowserRouter>
+      <HashRouter>
           <Routes>
               <Route path="/my-portfolio/home" element={ <Home/>} />
               <Route path="/my-portfolio/aboutme" element={ <AboutMe/>} />
               <Route path="/" element={<Home/> } />
               <Route path="*" element={ <Home/>} />
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
